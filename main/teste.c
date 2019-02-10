2a342f121bdc6a74e8733717ae83849b9f76e632
Aivar Koodi <aivar.koodi@gmail.com>
2019-02-10 18:58:09 +0200

    init repo

---
+++ main/teste.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : teste.c
+ Author      : Aivar Koodi
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("hello"); /* prints hello */
+	return EXIT_SUCCESS;
+}
